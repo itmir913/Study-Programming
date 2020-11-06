b350c5ec9e5bfe756a0ca79b47752ad5d42b95b7
itmir913 <itmir913@gmail.com>
2020-11-06 18:18:41 +0900

    Code Up 1030 by C

---
+++ 1030_15643237(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+
+//    double d;
+//    scanf("%lf", &d);
+//    printf("%.11lf", d);
+
+    return 0;
+}
+
