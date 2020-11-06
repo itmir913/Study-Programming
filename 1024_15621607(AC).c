96de98aa1df85b2a197de5a052da2b39c052f9a5
itmir913 <itmir913@gmail.com>
2020-11-06 18:18:37 +0900

    Code Up 1024 by C

---
+++ 1024_15621607(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char a[30];
+    scanf("%s", a);
+
+    int i;
+    for (i = 0; a[i] != '\0'; i++) {
+        printf("\'%c\'\n", a[i]);
+    }
+
+    return 0;
+}
+
