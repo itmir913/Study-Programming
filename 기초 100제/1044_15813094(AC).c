bc2c0bd5642eaa0b6add58df9c485f4281b7a65e
itmir913 <itmir913@gmail.com>
2020-11-08 01:25:15 +0900

    Code Up 1044 by C

---
+++ 기초 100제/1044_15813094(AC).c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    /**
+     * 입력 값의 범위가 -2147483648 ~ +2147483647 이므로 long long int로 받아준다.
+     */
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", ++n);
+
+    return 0;
+}
