5b9f815229c241271389a84b3e9c6268a5105967
itmir913 <itmir913@gmail.com>
2020-11-08 01:30:33 +0900

    Code Up 1046 by C

---
+++ 기초 100제/1046_15813119(AC).c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int n, m, l;
+    scanf("%lld %lld %lld", &n, &m, &l);
+    long long int sum = n + m + l;
+    printf("%lld\n", sum);
+    printf("%.1f\n", (float) sum / 3);
+
+    return 0;
+}
+
