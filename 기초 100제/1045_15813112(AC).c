b74589d6ec523903300144fca18de16286d1f758
itmir913 <itmir913@gmail.com>
2020-11-08 01:30:32 +0900

    Code Up 1045 by C

---
+++ 기초 100제/1045_15813112(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int n, m;
+    scanf("%lld %lld", &n, &m);
+    printf("%lld\n", n + m);
+    printf("%lld\n", n - m);
+    printf("%lld\n", n * m);
+    printf("%lld\n", (long long int) n / m);
+    printf("%lld\n", (long long int) n % m);
+    printf("%.2f\n", (float) n / m);
+
+    return 0;
+}
+
