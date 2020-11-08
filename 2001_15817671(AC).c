e261c0a35b0663d8de9055b2d6ee1ccc9fd72dad
itmir913 <itmir913@gmail.com>
2020-11-08 18:08:57 +0900

    Code Up 2001 by C

---
+++ 2001_15817671(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a, b, c, d, e;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    scanf("%d", &e);
+
+    int pasta = (a < b ? a : b) < c ? (a < b ? a : b) : c;
+    int ju = d < e ? d : e;
+
+    printf("%.1f", (pasta + ju) * 1.1);
+
+    return 0;
+}
+
