5f51ce7446a45b5f33f294b9ccbcc921df871534
itmir913 <itmir913@gmail.com>
2020-11-08 21:11:11 +0900

    Code Up 1075 by C

---
+++ 기초 100제/1075_15819368(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n;
+    scanf("%d", &n);
+
+    while (n > 0) {
+        n -= 1;
+        printf("%d\n", n);
+    }
+
+    return 0;
+}
+
