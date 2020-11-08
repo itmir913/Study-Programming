d721516be924507652d782d92f694efc0cff100b
itmir913 <itmir913@gmail.com>
2020-11-08 18:08:57 +0900

    Code Up 1164 by C

---
+++ 1164_15817523(AC).c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int aa = 1, bb = 1, cc = 1;
+    if (a <= 170)
+        aa = 0;
+
+    if (b <= 170)
+        bb = 0;
+
+    if (c <= 170)
+        cc = 0;
+
+    if (aa && bb && cc)
+        printf("PASS\n");
+    else
+        printf("CRASH\n");
+
+    return 0;
+}
+
