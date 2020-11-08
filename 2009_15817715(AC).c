311cbf57a8eda024e974d705e20ca4d4aca9ef8e
itmir913 <itmir913@gmail.com>
2020-11-08 18:08:57 +0900

    Code Up 2009 by C

---
+++ 2009_15817715(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int k, n;
+    scanf("%d %d", &k, &n);
+
+    int num = 0;
+    while (k >= n) {
+        k -= n;
+        num++;
+        k++;
+    }
+
+    printf("%d", num);
+
+    return 0;
+}
+
