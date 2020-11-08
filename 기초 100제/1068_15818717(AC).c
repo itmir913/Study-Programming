93adee84cf0d8fce37e4b03663ffe99e9e3216d4
itmir913 <itmir913@gmail.com>
2020-11-08 20:08:13 +0900

    Code Up 1068 by C

---
+++ 기초 100제/1068_15818717(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 90) {
+        printf("A\n");
+    } else if (n >= 70) {
+        printf("B\n");
+    } else if (n >= 40) {
+        printf("C\n");
+    } else {
+        printf("D\n");
+    }
+
+    return 0;
+}
+
