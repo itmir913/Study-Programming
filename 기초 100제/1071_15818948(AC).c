aa2fe1e09554fa7d60cd2522f2a1896d3b7eba1f
itmir913 <itmir913@gmail.com>
2020-11-08 21:11:11 +0900

    Code Up 1071 by C

---
+++ 기초 100제/1071_15818948(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n;
+
+    lable:
+    scanf("%d", &n);
+    if (n != 0) {
+        printf("%d\n", n);
+        goto lable;
+    }
+
+    return 0;
+}
+
