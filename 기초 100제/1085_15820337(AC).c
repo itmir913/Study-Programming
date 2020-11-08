8be2e63ebf3afc68806ab80081c83f8b134f1a91
itmir913 <itmir913@gmail.com>
2020-11-08 22:31:34 +0900

    Code Up 1085 by C

---
+++ 기초 100제/1085_15820337(AC).c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int h, b, c, s;
+    scanf("%lld %lld %lld %lld", &h, &b, &c, &s);
+
+    printf("%.1f MB", (float) (h * b * c * s) / (1024 * 1024 * 8));
+
+    return 0;
+}
+
