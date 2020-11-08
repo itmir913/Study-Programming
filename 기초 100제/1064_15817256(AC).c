4f9a495483c1fc021c67c19245980ffd94c1ed5f
itmir913 <itmir913@gmail.com>
2020-11-08 17:20:59 +0900

    Code Up 1064 by C

---
+++ 기초 100제/1064_15817256(AC).c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, m, l;
+    scanf("%d %d %d", &n, &m, &l);
+    printf("%d", (n < m ? n : m) < l ? (n < m ? n : m) : l);
+
+    return 0;
+}
+
