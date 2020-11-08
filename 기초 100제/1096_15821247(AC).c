fe8cfe98b01dcfec06a185bce843e964d30b38d4
itmir913 <itmir913@gmail.com>
2020-11-08 23:36:42 +0900

    Code Up 1096 by C

---
+++ 기초 100제/1096_15821247(AC).c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n;
+    scanf("%d", &n);
+
+    int a[19][19] = {};
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        a[x - 1][y - 1] = 1;
+    }
+
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 19; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
