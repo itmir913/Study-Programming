fc61f7692afeddd159f4a3faaab816a1ea7069b4
itmir913 <itmir913@gmail.com>
2020-11-08 15:44:53 +0900

    Code Up 1056 by C

---
+++ 기초 100제/1056_15816297(AC).c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    printf("%d\n", (n && !m) || (!n && m)); // 비트 xor 연산자는 ^인데, 논리 xor 연산자는 없는 듯
+
+    return 0;
+}
+
