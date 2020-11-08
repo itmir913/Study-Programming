79943a91cf2681c4ccab543a57207ef078446750
itmir913 <itmir913@gmail.com>
2020-11-08 18:18:58 +0900

    Code Up 1067 by C

---
+++ 기초 100제/1067_15817810(AC).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int n;
+    scanf("%lld", &n);
+
+    if (n > 0) {
+        printf("plus\n");
+    } else {
+        printf("minus\n");
+    }
+
+    if (n % 2 == 0) {
+        printf("even\n");
+    } else {
+        printf("odd\n");
+    }
+
+    return 0;
+}
+
