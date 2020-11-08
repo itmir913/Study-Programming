07e01b1ccc05c582921412c36fb96682c39cb1f7
itmir913 <itmir913@gmail.com>
2020-11-09 00:00:11 +0900

    Code Up 1098 by C

---
+++ 기초 100제/1098_15821446(AC).c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int h, w;
+    scanf("%d %d", &h, &w);
+
+    int a[100][100] = {};
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        int l, d, x, y;
+        scanf("%d %d %d %d", &l, &d, &x, &y);
+
+        if (d == 0) {
+            for (int j = 0; j < l; ++j) {
+                a[x - 1][y - 1] = 1;
+                y++;
+            }
+        } else if (d == 1) {
+            for (int j = 0; j < l; ++j) {
+                a[x - 1][y - 1] = 1;
+                x++;
+            }
+        }
+    }
+
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
