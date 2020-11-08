dcbb1dd59ea18aa013da855bcaa8b187ccf34819
itmir913 <itmir913@gmail.com>
2020-11-08 22:04:31 +0900

    Code Up 1084 by C

---
+++ 기초 100제/1084_15820003(AC).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int r, g, b;
+    scanf("%d %d %d", &r, &g, &b);
+
+    int sum = 0;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < g; j++) {
+            for (int k = 0; k < b; k++) {
+                printf("%d %d %d\n", i, j, k);
+                sum++;
+            }
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
