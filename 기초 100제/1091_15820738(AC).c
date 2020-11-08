958e73712aa367935f15c70b04f797fed8dc630e
itmir913 <itmir913@gmail.com>
2020-11-08 23:05:05 +0900

    Code Up 1091 by C

---
+++ 기초 100제/1091_15820738(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int a, m, d, n;
+    scanf("%lld %lld %lld %lld", &a, &m, &d, &n);
+
+    for (int i = 1; i < n; i++) {
+        a = (a * m) + d;
+    }
+
+    printf("%lld\n", a);
+
+    return 0;
+}
+
