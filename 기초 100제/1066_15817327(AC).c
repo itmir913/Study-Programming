d09e6075985256c49b4b80ff335cd748473bd92c
itmir913 <itmir913@gmail.com>
2020-11-08 17:42:35 +0900

    Code Up 1066 by C

---
+++ 기초 100제/1066_15817327(AC).c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, m, l;
+    scanf("%d %d %d", &n, &m, &l);
+    printf("%s\n", n % 2 == 0 ? "even" : "odd");
+    printf("%s\n", m % 2 == 0 ? "even" : "odd");
+    printf("%s\n", l % 2 == 0 ? "even" : "odd");
+
+    return 0;
+}
+
