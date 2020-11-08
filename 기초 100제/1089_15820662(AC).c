5bc590e42ad829c23f707a8adf105040b4f459a1
itmir913 <itmir913@gmail.com>
2020-11-08 23:05:05 +0900

    Code Up 1089 by C

---
+++ 기초 100제/1089_15820662(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a, d, n;
+    scanf("%d %d %d", &a, &d, &n);
+
+    for (int i = 1; i < n; i++) {
+        a += d;
+    }
+
+    printf("%d\n", a);
+
+    return 0;
+}
+
