86a996f218cf8ad25e6d3764cab7a5f40845d96e
itmir913 <itmir913@gmail.com>
2020-11-08 21:45:01 +0900

    Code Up 1079 by C

---
+++ 기초 100제/1079_15819654(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char n = '\0';
+
+    while (n != 'q') {
+        scanf("%c", &n);
+        printf("%c", n);
+        if (n == 'q') {
+            break;
+        }
+    }
+
+    return 0;
+}
+
