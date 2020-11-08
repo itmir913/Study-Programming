6b2351cbb0faa4ffa2b0bf56ee9df6d69eb4d52f
itmir913 <itmir913@gmail.com>
2020-11-08 18:08:57 +0900

    Code Up 1165 by C

---
+++ 1165_15817556(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    while(a < 90){
+        b++;
+        a+=5;
+    }
+
+    printf("%d", b);
+
+    return 0;
+}
+
