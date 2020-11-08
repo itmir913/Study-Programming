f31a12c145d0f9e9cf5fe02b69c7f3d98662c2df
itmir913 <itmir913@gmail.com>
2020-11-08 23:31:29 +0900

    Code Up 1095 by C

---
+++ 기초 100제/1095_15821203(AC).c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, t, min = -1;
+    scanf("%d", &n);
+
+    int a[10000] = {};
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &t);
+        a[i] = t;
+        if ((min == -1) || (t < min)) {
+            min = t;
+        }
+    }
+
+    printf("%d ", min);
+
+    return 0;
+}
+
