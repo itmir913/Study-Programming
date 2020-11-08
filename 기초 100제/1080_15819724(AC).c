8418546153fcb9bf48119e81bfaea361847853ca
itmir913 <itmir913@gmail.com>
2020-11-08 21:45:02 +0900

    Code Up 1080 by C

---
+++ 기초 100제/1080_15819724(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n;
+    scanf("%d", &n);
+
+    int i = 1, sum = 0;
+    while (sum <= n) {
+        sum += i;
+        if (sum < n) {
+            i++;
+        }
+    }
+
+    printf("%d", i);
+
+    return 0;
+}
+
