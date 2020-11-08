652016937004396e1df18a803c22c28479bb0cd4
itmir913 <itmir913@gmail.com>
2020-11-08 23:05:05 +0900

    Code Up 1090 by C

---
+++ 기초 100제/1090_15820685(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int a, r, n;
+    scanf("%lld %lld %lld", &a, &r, &n);
+
+    for (int i = 1; i < n; i++) {
+        a *= r;
+    }
+
+    printf("%lld\n", a);
+
+    return 0;
+}
+
