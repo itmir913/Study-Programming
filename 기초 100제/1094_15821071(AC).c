1d41444ec698dcb1a02c711b66060e32144cc649
itmir913 <itmir913@gmail.com>
2020-11-08 23:21:19 +0900

    Code Up 1094 by C

---
+++ 기초 100제/1094_15821071(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, t;
+    scanf("%d", &n);
+
+    int a[10000] = {};
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &t);
+        a[i] = t;
+    }
+
+    for (int i = n - 1; i >= 0; --i) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
