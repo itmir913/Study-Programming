3777e44bbec77eb249728c2f9a136c7645ddc32f
itmir913 <itmir913@gmail.com>
2020-11-09 00:17:08 +0900

    Code Up 1099 by C

---
+++ 기초 100제/1099_15821563(AC).c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a[10][10] = {};
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int x = 2 - 1, y = 2 - 1;
+
+    while (1) {
+        if (a[x][y] == 2) {
+            a[x][y] = 9;
+            break;
+        }
+
+        a[x][y] = 9;
+
+        if (a[x][y + 1] != 1) {
+            y++;
+        } else if (a[x + 1][y] != 1) {
+            x++;
+        } else {
+            break;
+        }
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
