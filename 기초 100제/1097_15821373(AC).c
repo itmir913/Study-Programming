0b830103c96f722d8e2e95c1eec8a57134e9764c
itmir913 <itmir913@gmail.com>
2020-11-08 23:52:07 +0900

    Code Up 1097 by C

---
+++ 기초 100제/1097_15821373(AC).c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a[19][19] = {};
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 19; ++j) {
+            int t;
+            scanf("%d", &t);
+            a[i][j] = t;
+        }
+    }
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        for (int j = 0; j < 19; ++j) {
+            a[x-1][j] = !a[x-1][j];
+        }
+
+        for (int k = 0; k < 19; ++k) {
+            a[k][y-1] = !a[k][y-1];
+        }
+
+    }
+
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 19; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
