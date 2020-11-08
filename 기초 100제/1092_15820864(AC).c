00d4bd2e8b0bd063d4198744305fe818c0c2a97e
itmir913 <itmir913@gmail.com>
2020-11-08 23:05:05 +0900

    Code Up 1092 by C

---
+++ 기초 100제/1092_15820864(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int a, b, c, i = 1;
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    while (1) {
+        if ((i % a == 0) && (i % b == 0) && (i % c == 0)) {
+            break;
+        }
+        i++;
+    }
+
+    printf("%lld\n", i);
+
+    return 0;
+}
+
