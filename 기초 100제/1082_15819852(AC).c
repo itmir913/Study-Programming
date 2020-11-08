251d3f44610489f3cbc434c51cf9423dd3de84ae
itmir913 <itmir913@gmail.com>
2020-11-08 21:49:52 +0900

    Code Up 1082 by C

---
+++ 기초 100제/1082_15819852(AC).c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, m = 0xF;
+    scanf("%x", &n);
+
+    for (int i = 1; i <= m; i++) {
+        printf("%X*%X=%X\n", n, i, n * i);
+    }
+
+    return 0;
+}
+
