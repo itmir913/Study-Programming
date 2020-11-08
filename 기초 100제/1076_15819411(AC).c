9b25cddf47e0ed13f4545c71cb6bd9bf813aac40
itmir913 <itmir913@gmail.com>
2020-11-08 21:11:11 +0900

    Code Up 1076 by C

---
+++ 기초 100제/1076_15819411(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char n, m='a';
+    scanf("%c", &n);
+
+    do {
+        printf("%c\n", m);
+        m++;
+    }while (m <= n);
+
+    return 0;
+}
+
