6daecd9fcc1aa52c8f2c19e2250800cfc12752e6
itmir913 <itmir913@gmail.com>
2020-11-08 17:42:35 +0900

    Code Up 1065 by C

---
+++ 기초 100제/1065_15817300(AC).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, m, l;
+    scanf("%d %d %d", &n, &m, &l);
+
+    if (n % 2 == 0) {
+        printf("%d\n", n);
+    }
+
+    if (m % 2 == 0) {
+        printf("%d\n", m);
+    }
+
+    if (l % 2 == 0) {
+        printf("%d\n", l);
+    }
+
+    return 0;
+}
+
