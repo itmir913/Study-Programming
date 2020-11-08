26366175443c185f0750c0afc0d68ff29a2d528a
itmir913 <itmir913@gmail.com>
2020-11-08 21:45:01 +0900

    Code Up 1078 by C

---
+++ 기초 100제/1078_15819506(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    int sum = 0;
+    int n, i;
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        if (i % 2 == 0)
+            sum += i;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
