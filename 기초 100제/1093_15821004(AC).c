f328d7775f193141006318207061d119e2a2f03c
itmir913 <itmir913@gmail.com>
2020-11-08 23:15:53 +0900

    Code Up 1093 by C

---
+++ 기초 100제/1093_15821004(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int n, t;
+    scanf("%d", &n);
+
+    int a[24] = {};
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &t);
+        a[t-1] += 1;
+    }
+
+    for (int i = 0; i < 23; ++i) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
