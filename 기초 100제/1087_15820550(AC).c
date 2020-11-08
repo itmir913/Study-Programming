d476a5ce20561d1741fb51f5b954e7a7f0eac5c7
itmir913 <itmir913@gmail.com>
2020-11-08 23:05:05 +0900

    Code Up 1087 by C

---
+++ 기초 100제/1087_15820550(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    long long int n, sum = 0;
+    scanf("%lld", &n);
+
+    for (int i = 0; sum < n; i++) {
+        sum += i;
+    }
+
+    printf("%lld\n", sum);
+
+    return 0;
+}
+
