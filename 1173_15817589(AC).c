75ceb931ee9e28cd6a92330bf1b7018dc9e244ad
itmir913 <itmir913@gmail.com>
2020-11-08 18:08:57 +0900

    Code Up 1173 by C

---
+++ 1173_15817589(AC).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int h, m;
+    scanf("%d %d", &h, &m);
+
+    m -= 30;
+    if (m < 0) {
+        h--;
+        m += 60;
+    }
+
+    if (h < 0) {
+        h += 24;
+    }
+
+    printf("%d %d", h, m);
+
+    return 0;
+}
+
