47428c075a67889a59811e64aa49006b7f06cf0d
itmir913 <itmir913@gmail.com>
2020-11-17 21:53:52 +0900

    Code Up 1135 by C

---
+++ Code Up/1135_15976504(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a >= b) {
+        printf("1");
+    } else {
+        printf("0");
+    }
+
+    return 0;
+}
+
