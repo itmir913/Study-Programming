602333989746c95bc7591c097a37ad6704cf9c67
itmir913 <itmir913@gmail.com>
2020-11-17 21:59:47 +0900

    Code Up 1166 by C

---
+++ Code Up/1166_15976593(AC).c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int year_function(int year);
+
+int main(int argc, char **argv) {
+    int year;
+    scanf("%d", &year);
+
+    switch (year_function(year)) {
+        case 1:
+            printf("yes");
+            break;
+        case 0:
+            printf("no");
+    }
+
+    return 0;
+}
+
+int year_function(int year) {
+    if (year % 4 == 0 && year % 100 != 0) {
+        return 1;
+    } else if (year % 400 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
