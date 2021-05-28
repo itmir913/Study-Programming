c318a4d1f07ed8e20f89c7bcdba489c878c54efa
itmir913 <itmir913@gmail.com>
2021-05-28 22:43:48 +0900

    Code Up 1402 by C

---
+++ Study-C/Code Up/1402_19431665(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    int *array = (int *) malloc(sizeof(int) * num);
+
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = num - 1; i >= 0; i--) {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
+
