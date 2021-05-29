ae60e4f31e933f94ecbb10311c358e4cd9c4a493
itmir913 <itmir913@gmail.com>
2021-05-29 15:56:38 +0900

    Code Up 1230 by C

---
+++ Study-C/Code Up/1230_19439428(AC).c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int height[3];
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &height[i]);
+    }
+
+    int my_car = 170;
+    int check = 0;
+    for (int i = 0; i < 3; ++i) {
+        if (my_car >= height[i]) {
+            printf("CRASH %d", height[i]);
+            check = 1;
+            break;
+        }
+    }
+
+    if (check == 0) {
+        printf("PASS");
+    }
+
+    return 0;
+}
