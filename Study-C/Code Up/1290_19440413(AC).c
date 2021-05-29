7029387250b5ba3fa40b86bbc2a6da1d9484efe9
itmir913 <itmir913@gmail.com>
2021-05-29 16:52:51 +0900

    Code Up 1290 by C

---
+++ Study-C/Code Up/1290_19440413(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+
+    int count = 0;
+    for (int i = 1; i <= (int) a / 2; ++i) {
+        if (a % i == 0)
+            count++;
+    }
+
+    printf("%d ", count);
+
+    return 0;
+}
