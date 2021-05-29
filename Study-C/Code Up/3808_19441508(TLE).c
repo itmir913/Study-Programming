556aac82beb0f5e1eb80ba96b4959f1f6ba5aa27
itmir913 <itmir913@gmail.com>
2021-05-29 19:00:40 +0900

    Code Up 3808 Time Out

---
+++ Study-C/Code Up/3808_19441508(TLE).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int derangement(int n) { // n>= 2
+    if (n == 0) return 1; // D0 = 1
+    else if (n == 1) return 0; // D1 = 0
+    return (n - 1) * (derangement(n - 1) + derangement(n - 2));
+}
+
+int main(void) {
+    int num;
+    scanf("%d", &num);
+
+    printf("%d", derangement(num));
+
+    return 0;
+}
