5d96947beced295d27e7b3cbfc79b6a8c14993e1
itmir913 <itmir913@gmail.com>
2021-05-29 17:04:11 +0900

    Code Up 1292 by C

---
+++ Study-C/Code Up/1292_19440595(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    char input[10];
+    scanf("%s", input);
+
+    int sum = 0;
+    for (int i = 0; i < 10; ++i) {
+        sum += *(input + i) - 48;
+    }
+
+    if (sum % 7 == 4) printf("suspect");
+    else printf("citizen");
+
+    return 0;
+}
