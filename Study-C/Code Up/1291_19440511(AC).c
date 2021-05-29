8a1d129651d569abf456b4d2ec655439bac2925b
itmir913 <itmir913@gmail.com>
2021-05-29 17:04:11 +0900

    Code Up 1291 by C

---
+++ Study-C/Code Up/1291_19440511(AC).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int a, b, c, min;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b) min = b;
+    else min = a;
+
+    if (c < min) min = c;
+
+    int vaccine = 1;
+    for (int i = 1; i <= min; ++i) {
+        if ((a % i == 0) && (b % i == 0) && (c % i == 0))
+            vaccine = i;
+    }
+
+    printf("%d ", vaccine);
+
+    return 0;
+}
