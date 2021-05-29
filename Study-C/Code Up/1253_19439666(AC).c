a02d23b2623a2a545f0ab84385b1a05cd63590a5
itmir913 <itmir913@gmail.com>
2021-05-29 16:08:45 +0900

    Code Up 1253 by C

---
+++ Study-C/Code Up/1253_19439666(AC).c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (int i = a; i <= b; ++i) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
