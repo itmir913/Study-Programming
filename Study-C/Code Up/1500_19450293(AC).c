4f419920044e2e153171289b485b2be540eb620c
itmir913 <itmir913@gmail.com>
2021-05-30 13:20:15 +0900

    Code Up 1500 by C

---
+++ Study-C/Code Up/1500_19450293(AC).c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int **createMatrix(int height, int width) { // https://codeng.tistory.com/8
+    int **arr;
+
+    arr = (int **) malloc(sizeof(int *) * height);
+    arr[0] = (int *) malloc(sizeof(int) * width * height);
+
+    for (int i = 1; i < height; i++) {
+        arr[i] = arr[i - 1] + width;
+    }
+
+    return arr;
+}
+
+int main(void) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int **aa = createMatrix(n, m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &aa[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", aa[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(aa[0]);
+    free(aa);
+
+    return 0;
+}
+
