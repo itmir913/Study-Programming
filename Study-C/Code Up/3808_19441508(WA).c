d503eee46e40cb6a934694dfb1eabdb725a43faa
itmir913 <itmir913@gmail.com>
2021-05-30 13:12:25 +0900

    Code Up 3808 Tests Pass by C

--- Study-C/Code Up/3808_19441508(WA).c
+++ Study-C/Code Up/3808_19441508(WA).c
@@ -3,22 +3,22 @@
 
 #define MEMOIZATION 2000000
 
-int derangement(int n, int *arr) { // n>= 2
+unsigned long long derangement(int n, unsigned long long *arr) { // n>= 2
     if (n == 0 || n == 1) {
         return arr[n]; // D0 = 1, D1 = 0
     } else {
-        int n_minus_1, n_minus_2;
+        unsigned long long n_minus_1, n_minus_2;
 
-        if (arr[n - 2] == -1)
+        if (arr[n - 2] == 0)
             arr[n - 2] = derangement(n - 2, arr);
 
-        if (arr[n - 1] == -1)
+        if (arr[n - 1] == 0)
             arr[n - 1] = derangement(n - 1, arr);
 
-        n_minus_1 = arr[n - 1];
-        n_minus_2 = arr[n - 2];
+        n_minus_1 = arr[n - 1] % 1000000007;
+        n_minus_2 = arr[n - 2] % 1000000007;
 
-        arr[n] = (n - 1) * (n_minus_1 + n_minus_2);
+        arr[n] = (n - 1) * (n_minus_1 + n_minus_2) % 1000000007;
 
         return arr[n];
     }
@@ -28,16 +28,15 @@ int main(void) {
     int num;
     scanf("%d", &num);
 
-    int *arr = (int *) malloc(sizeof(int) * MEMOIZATION);
+    unsigned long long *arr = (unsigned long long *) malloc(sizeof(unsigned long long) * MEMOIZATION);
 
     arr[0] = 1;
     arr[1] = 0;
     for (int i = 2; i < MEMOIZATION; i++) {
-        *(arr + i) = -1;
+        *(arr + i) = 0;
     }
 
-    printf("%d", derangement(num, arr) % 1000000007);
+    printf("%llu", derangement(num, arr));
 
     return 0;
 }
-
