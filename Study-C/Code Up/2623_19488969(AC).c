a3f56ba3ae3ea58e7218505e43661a8a877f5d8c
itmir913 <itmir913@gmail.com>
2021-05-31 19:13:36 +0900

    Code Up 2623 by C

---
+++ Study-C/Code Up/2623_19488969(AC).c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int gcd(int a, int b) { // GCD 알고리즘
+    if (a < b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    int n;
+    while (b != 0) {
+        n = a % b;
+        a = b;
+        b = n;
+    }
+
+    return a;
+}
+
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", gcd(a, b));
+
+    return 0;
+}
+
