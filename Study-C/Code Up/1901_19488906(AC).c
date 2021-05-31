cad7766e88de0083110156dda9909432c184902e
itmir913 <itmir913@gmail.com>
2021-05-31 19:09:57 +0900

    Code Up 1901 by C

---
+++ Study-C/Code Up/1901_19488906(AC).c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void fact(int count, int n) {
+    printf("%d\n", count);
+    if (count < n)
+        fact(count + 1, n);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    fact(1, n);
+
+    return 0;
+}
+
