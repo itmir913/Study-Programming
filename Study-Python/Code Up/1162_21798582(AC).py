cb464ff05b15102e4b6c93348c437e368314282c
itmir913 <itmir913@gmail.com>
2021-10-06 22:27:26 +0900

    Code Up 1162 by Python

---
+++ Study-Python/Code Up/1162_21798582(AC).py
@@ -0,0 +1,17 @@
+﻿"""
+1162 당신의 사주를 봐 드립니다 1
+"""
+
+
+def main():
+    year, month, day = map(int, input().split())
+
+    calc = (year - month + day) % 10
+    if calc == 0:
+        print("대박")
+    else:
+        print("그럭저럭")
+
+
+main()
+
