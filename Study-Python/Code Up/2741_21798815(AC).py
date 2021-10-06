28bee826d4020e79ba21e66c2a651ec61da77aba
itmir913 <itmir913@gmail.com>
2021-10-06 23:16:09 +0900

    Code Up 2741_21798815(AC).py by Python

---
+++ Study-Python/Code Up/2741_21798815(AC).py
@@ -0,0 +1,16 @@
+﻿"""
+2741 : 게임 중독
+"""
+
+
+def main():
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    mystr = list(input())
+    crypto = input()
+
+    for a in crypto:
+        print(alphabet[mystr.index(a)], end="")
+
+
+main()
+
