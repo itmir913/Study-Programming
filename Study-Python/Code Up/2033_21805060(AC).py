9b886516c1f84f1ed2ca763eb0db0030bd1965e0
itmir913 <itmir913@gmail.com>
2021-10-07 13:04:22 +0900

    Code Up 2033_21805060(AC).py by Python

---
+++ Study-Python/Code Up/2033_21805060(AC).py
@@ -0,0 +1,31 @@
+﻿"""
+2033: 사다리게임
+"""
+
+
+def main():
+    k, n = map(int, input().split())
+
+    connected = []
+    for a in range(n):
+        connected.append(list(map(int, input().split())))
+
+    start = int(input())
+    location = start
+
+    for line in connected:
+        if location not in line:
+            continue
+
+        index = line.index(location)
+
+        if index == 0:
+            location = line[1]
+        elif index == 1:
+            location = line[0]
+
+    print(location)
+
+
+main()
+
