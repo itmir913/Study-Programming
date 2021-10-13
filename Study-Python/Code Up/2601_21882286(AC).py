c997c867fcb7020d686ea5f66d0f7ae8943d314c
itmir913 <itmir913@gmail.com>
2021-10-13 09:19:54 +0900

    Code Up 2601_21882286(AC).py by Python

---
+++ Study-Python/Code Up/2601_21882286(AC).py
@@ -0,0 +1,20 @@
+﻿# 1165 : 축구의신
+
+import sys
+
+n = int(sys.stdin.readline())
+
+memo = {1: 1, 2: 1}
+
+
+def fibonacci(n):
+    if n in memo:
+        return memo[n]
+
+    result = fibonacci(n - 1) + fibonacci(n - 2)
+    memo[n] = result
+    return result
+
+
+print(fibonacci(n))
+
