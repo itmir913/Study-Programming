8d853d5725a093db67c6bfdd48ce1ba57630dedb
itmir913 <itmir913@gmail.com>
2021-10-13 15:56:36 +0900

    Code Up 3708_21892332(AC).py by Python

---
+++ Study-Python/Code Up/3708_21892332(AC).py
@@ -0,0 +1,33 @@
+﻿# 3708: nCr (Large)
+
+import sys
+
+n, r = map(int, sys.stdin.readline().split())
+
+memo = {0: 1,
+        1: 1}
+
+
+def factorial(n):
+    if n in memo:
+        return memo[n]
+    elif n - 1 in memo:
+        result = (memo[n - 1] * n)
+        memo[n] = result
+        return result
+
+    result = 1
+    for x in range(1, (n) + 1):
+        if x in memo:
+            result = memo[x]
+            continue
+        else:
+            result = (result * x)
+            memo[x] = result
+
+    return result
+
+
+nCr = (factorial(n) // (factorial(r) * factorial(n - r))) % 100000007
+print(nCr)
+
